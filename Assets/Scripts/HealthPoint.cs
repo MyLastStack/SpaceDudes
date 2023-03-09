653a5a4ed9ab1bb7e81ddea4c782b50d5ff3dd42
Johnne Geograpo <59752069+MyLastStack@users.noreply.github.com>
2023-03-09 05:09:28 -0700

    finished

--- Assets/Scripts/HealthPoint.cs
+++ Assets/Scripts/HealthPoint.cs
@@ -21,14 +21,22 @@ public class HealthPoint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        HealthUpdate();
     }
 
     void HealthUpdate()
     {
         if (playerBehaviour.PlayerHP == 2)
         {
-            
+            Destroy(healthPoint3);
+        }
+        if (playerBehaviour.PlayerHP == 1)
+        {
+            Destroy(healthPoint2);
+        }
+        if (playerBehaviour.PlayerHP == 0)
+        {
+            Destroy(healthPoint);
         }
     }
 }
