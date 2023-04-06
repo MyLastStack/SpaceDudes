3e69f45a89746439d404d614b5dafd68d087a8cd
Johnne Geograpo <59752069+MyLastStack@users.noreply.github.com>
2023-04-06 06:54:49 -0600

    reformat

--- Assets/Scripts/GameSceneManager.cs
+++ Assets/Scripts/GameSceneManager.cs
@@ -5,9 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class GameSceneManager : MonoBehaviour
 {
-    public static void LoadMainMenu() { SceneManager.LoadScene(0); }
-    public static void LoadLevelOne() { SceneManager.LoadScene(1); }
-    public static void LoadLevelTwo() { SceneManager.LoadScene(2); }
-    public static void LoadWinScreen() { SceneManager.LoadScene(3); }
-    public static void LoadLoseScreen() { SceneManager.LoadScene(4); }
+    int currentScene = 0;
+    public static void LoadMainMenu() 
+    { 
+        SceneManager.LoadScene(0); 
+    }
+    public static void LoadLevelOne() 
+    { 
+        SceneManager.LoadScene(1); 
+    }
+    public static void LoadLevelTwo() 
+    { 
+        SceneManager.LoadScene(2); 
+    }
+    public static void LoadWinScreen() 
+    { 
+        SceneManager.LoadScene(3); 
+    }
+    public static void LoadLoseScreen() 
+    { 
+        SceneManager.LoadScene(4); 
+    }
 }
