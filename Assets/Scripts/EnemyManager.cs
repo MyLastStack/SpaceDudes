42603879769feed60cb4f1205b4657d646f27c4c
Johnne Geograpo <59752069+MyLastStack@users.noreply.github.com>
2023-04-06 06:48:16 -0600

    ui update

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -6,17 +6,31 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI EnemiesLeft;
-    public int EnemyCount;
+    [SerializeField] TextMeshProUGUI Score;
+
+    protected GameState gameState;
+    int MaxEnemies;
+    int EnemyCount;
 
     // Start is called before the first frame update
     void Start()
     {
-        EnemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        MaxEnemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        gameState = GameObject.FindObjectOfType<GameState>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        EnemiesLeft.text = "Hi";
+        EnemiesLeft.text = $"Enemies\nleft: {EnemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length}";
+        if (EnemyCount < MaxEnemies)
+        {
+            Score.text = $"Score:\n{gameState.score = (MaxEnemies - EnemyCount) * 50}";
+        }
+
+        if (EnemyCount == 0)
+        {
+            GameSceneManager.LoadWinScreen();
+        }
     }
 }
