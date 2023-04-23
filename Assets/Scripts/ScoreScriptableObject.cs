f7f28bb6590bc4a04cb8a64ef95163d98ab174ec
Johnne Geograpo <59752069+MyLastStack@users.noreply.github.com>
2023-04-23 12:23:43 -0600

    Update ScoreScriptableObject.cs

--- Assets/Scripts/ScoreScriptableObject.cs
+++ Assets/Scripts/ScoreScriptableObject.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu]
 public class ScoreScriptableObject : ScriptableObject
 {
     [SerializeField] private float Score;
